5cf5e4dce6e80d79aec64d19b6cbc8949772c071
ppp <p@p.com>
2018-06-07 15:02:04 +0300

    add some metods and choise complication

--- sudoku/index.js
+++ sudoku/index.js
@@ -5,6 +5,8 @@ class Sudoku {
     this.arrItems = [];
     Sudoku.ARR_ROW = 9;
     Sudoku.ARR_COL = 9;
+    this.win = 0;
+    this.complication = 0.5;
   }
   render() {
     const table = document.createElement('table');
@@ -42,7 +44,7 @@ class Sudoku {
   someClear() {
     for (let i = 0; i < Sudoku.ARR_ROW; i++) {
       for (let j = 0; j < Sudoku.ARR_COL; j++) {
-        if (Math.random() > 0.5) {
+        if (Math.random() > this.complication) {
           this.arrItems[i][j] = '';
         }
       }
@@ -115,63 +117,58 @@ class Sudoku {
   rowChange() {
     const rnd = Math.floor(Math.random() * Sudoku.ARR_COL); // от 0 до 8
     const temp = [];
-    function getRnd() {
-      return Math.floor(Math.random() * 2 + 1); // 1 или 2
-    }
-    let rndDelta = getRnd();
-    function change() {
+
+    let rndDelta = Math.floor(Math.random() * 2 + 1);
+    const change = () => {
       for (let i = 0; i < Sudoku.ARR_COL; i++) {
         this.arrItems[rnd][i] = this.arrItems[rnd + rndDelta][i];
         this.arrItems[rnd + rndDelta][i] = temp[i];
       }
-    }
+    };
     for (let i = 0; i < Sudoku.ARR_COL; i++) {
       temp.push(this.arrItems[rnd][i]);
     }
     if (rnd === 0 || rnd === 3 || rnd === 6) {
-      change.call(this);
+      change();
     }
     if (rnd === 1 || rnd === 4 || rnd === 7) {
       if (rndDelta === 2) {
         rndDelta = -1;
       }
-      change.call(this);
+      change();
     }
     if (rnd === 2 || rnd === 5 || rnd === 8) {
       rndDelta *= -1;
-      change.call(this);
+      change();
     }
   }
   // меняем столбцы
   colChange() {
     const rnd = Math.floor(Math.random() * Sudoku.ARR_COL); // от 0 до 8
     const temp = [];
-    function getRnd() {
-      return Math.floor(Math.random() * 2 + 1); // 1 или 2
-    }
-    let rndDelta = getRnd();
-    function change() {
-      // вызываем
+
+    let rndDelta = Math.floor(Math.random() * 2 + 1);
+    const change = () => {
       for (let i = 0; i < Sudoku.ARR_COL; i++) {
         this.arrItems[i][rnd] = this.arrItems[i][rnd + rndDelta];
         this.arrItems[i][rnd + rndDelta] = temp[i];
       }
-    }
+    };
     for (let i = 0; i < Sudoku.ARR_COL; i++) {
       temp.push(this.arrItems[i][rnd]);
     }
     if (rnd === 0 || rnd === 3 || rnd === 6) {
-      change.call(this);
+      change();
     }
     if (rnd === 1 || rnd === 4 || rnd === 7) {
       if (rndDelta === 2) {
         rndDelta = -1;
       }
-      change.call(this);
+      change();
     }
     if (rnd === 2 || rnd === 5 || rnd === 8) {
       rndDelta *= -1;
-      change.call(this);
+      change();
     }
   }
   checkCorrectTable() {
@@ -179,12 +176,16 @@ class Sudoku {
       for (let j = 0; j < this.arrItems[i].length; j++) {
         const value = this.arrItems[i][j];
         if (value === '') {
+          this.error();
           return;
         }
         for (let t = j + 1; t < this.arrItems[i].length; t++) {
           if (this.arrItems[i][j] === this.arrItems[i][t]) {
+            this.error();
             return;
-          } else if (this.arrItems[j][i] === this.arrItems[t][i]) {
+          }
+          if (this.arrItems[j][i] === this.arrItems[t][i]) {
+            this.error();
             return;
           }
         }
@@ -199,58 +200,85 @@ class Sudoku {
           }
         }
         if (temp !== 45) {
+          this.error();
           return;
         }
       }
     }
-    alert('win');
+    this.gameWin();
   }
-}
-
-const sudoku = new Sudoku();
-sudoku.addNumberInTable();
-for (let i = 0, max = Math.random() * 100 + 100; i < max; i++) {
-  const rnd = Math.random();
-  if (rnd <= 0.33) {
-    sudoku.transparentTable();
-  } else if (rnd <= 0.66) {
-    sudoku.rowChange();
-  } else {
-    sudoku.colChange();
+  error() {
+    const errorField = document.querySelector('.errorField');
+    errorField.classList.add('show');
+    setTimeout(() => {
+      const errorField = document.querySelector('.errorField');
+      errorField.classList.remove('show');
+    }, 2000);
   }
-}
-
-document.getElementById('checkButton').addEventListener('click', () => {
-  sudoku.checkCorrectTable();
-});
-
-function inputValue(event) {
-  if (event.target.nodeName !== 'INPUT' && event.target.attributes.length !== 0) {
-    if (event.target.attributes[0].name === 'ready') {
-      const valueCell = event.target.textContent;
-      event.target.textContent = '';
-      const input = document.createElement('input');
-      input.value = valueCell;
-      event.target.appendChild(input);
-      input.focus();
-      input.addEventListener('blur', () => {
-        const value = event.target.firstChild.value;
-        event.target.firstChild.remove();
-        if (value >= 1 && value <= 9) {
-          event.target.textContent = value;
-          const i = event.toElement.parentElement.rowIndex;
-          const j = event.toElement.cellIndex;
-          sudoku.setValue(i, j, value);
-        } else {
-          event.target.textContent = '';
-        }
-      });
+  gameWin() {
+    this.win += 1;
+    alert('победа');
+    const countWin = document.querySelector('.countWin');
+    countWin.textContent = this.win;
+    this.prepareNewGame();
+  }
+  prepareNewGame() {
+    this.arrItems = [];
+    this.startGame();
+    const table = document.querySelector('.gameField');
+    table.textContent = '';
+    table.appendChild(this.render());
+    table.addEventListener('click', () => {
+      this.inputValue(event);
+    });
+  }
+  startGame() {
+    this.addNumberInTable();
+    for (let i = 0, max = Math.random() * 100 + 100; i < max; i++) {
+      const rnd = Math.random();
+      if (rnd <= 0.33) {
+        this.transparentTable();
+      } else if (rnd <= 0.66) {
+        this.rowChange();
+      } else {
+        this.colChange();
+      }
+    }
+    this.someClear();
+  }
+  inputValue(event) {
+    if (event.target.nodeName !== 'INPUT' && event.target.attributes.length !== 0) {
+      if (event.target.attributes[0].name === 'ready') {
+        const valueCell = event.target.textContent;
+        event.target.textContent = '';
+        const input = document.createElement('input');
+        input.value = valueCell;
+        event.target.appendChild(input);
+        input.focus();
+        input.addEventListener('blur', () => {
+          const value = event.target.firstChild.value;
+          event.target.firstChild.remove();
+          if (value >= 1 && value <= 9) {
+            event.target.textContent = value;
+            const i = event.toElement.parentElement.rowIndex;
+            const j = event.toElement.cellIndex;
+            this.setValue(i, j, value);
+          } else {
+            event.target.textContent = '';
+          }
+        });
+      }
     }
   }
 }
 
-sudoku.someClear();
-const table = document.body.appendChild(sudoku.render());
-table.addEventListener('click', () => {
-  inputValue(event);
+const sudoku = new Sudoku();
+sudoku.prepareNewGame();
+
+document.getElementById('checkButton').addEventListener('click', () => {
+  sudoku.checkCorrectTable();
+});
+document.querySelector('.complication').addEventListener('change', () => {
+  sudoku.complication = +event.target.value;
+  sudoku.prepareNewGame();
 });
